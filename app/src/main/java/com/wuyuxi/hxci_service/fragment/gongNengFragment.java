3d7b98a67cd85d018d8882cd97b48e9aadc6caac
Rabbittpig <49190898+Rabbittpig@users.noreply.github.com>
2019-04-23 10:03:25 +0800

    功能界面

--- app/src/main/java/com/wuyuxi/hxci_service/fragment/gongNengFragment.java
+++ app/src/main/java/com/wuyuxi/hxci_service/fragment/gongNengFragment.java
@@ -13,12 +13,13 @@ import com.wuyuxi.hxci_service.R;
 
 public class gongNengFragment extends Fragment {
     private TextView w_gongzuotai;
+    private TextView back;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.gn_gongren_layout, container, false);
         w_gongzuotai=view.findViewById(R.id.w_gongzuotai);
-
+        back=view.findViewById(R.id.back);
         return view;
     }
 }
