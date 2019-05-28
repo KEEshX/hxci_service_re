94504e0121da3ae2157f1a97aa5817d370a5816e
gaokexin <49713711+gaokexin@users.noreply.github.com>
2019-05-28 17:56:32 +0800

    高可心1

--- app/src/main/java/com/wuyuxi/hxci_service/fragment/woDeFragment.java
+++ app/src/main/java/com/wuyuxi/hxci_service/fragment/woDeFragment.java
@@ -1,5 +1,6 @@
 package com.wuyuxi.hxci_service.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -10,15 +11,37 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 import com.wuyuxi.hxci_service.R;
+
+import com.wuyuxi.hxci_service.Xiugaimima;
+import com.wuyuxi.hxci_service.login.LoginActivity;
 public class woDeFragment extends Fragment {
     private TextView yonghu;
     private Button btntuichu;
+    private TextView xiugainima;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.wode, container, false);
         yonghu=view.findViewById(R.id.yonghu);
         btntuichu=view.findViewById(R.id.btntuichu);
+        xiugainima=view.findViewById(R.id.xiugai);
+        xiugainima.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getActivity(),Xiugaimima.class);
+                startActivity(intent);
+            }
+        });
+        btntuichu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getActivity(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
         return view;
+
     }
+
 }
